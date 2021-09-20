4598177de5c078e8597e4f6a07daa68513e69cdc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-20 16:03:58 -0400

    day01

--- src/day01/OOP_Encapsulation.java
+++ src/day01/OOP_Encapsulation.java
@@ -2,23 +2,29 @@ package day01;
 
 public class OOP_Encapsulation {
 
-    public String name;
-    public double salary;
+    private String name;
+    private double salary;
 
     public String getName(){
         return name;
     }
 
     public void setName(String name){
+        if(name.isEmpty()){
+            throw new RuntimeException("Name cannot be empty");
+        }
         this.name = name;
     }
 
 
-    public double getSalary() {
+    public  double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
+    public  void setSalary(double salary) {
+        if(salary <= 0){
+            throw new RuntimeException("Salary can not be negative or zero");
+        }
         this.salary = salary;
     }
 
--- src/day01/Test.java
+++ src/day01/Test.java
@@ -1,4 +1,18 @@
 package day01;
 
 public class Test {
+
+    public static void main(String[] args) {
+
+        OOP_Encapsulation obj = new OOP_Encapsulation();
+      //  obj.name = "";
+     //   obj.salary = -10000;
+        obj.setName("Aaron");
+        obj.setSalary(10000);
+
+        System.out.println(obj.getName()+" : "+obj.getSalary());
+
+
+    }
+
 }
