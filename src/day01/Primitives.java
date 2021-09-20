e5a0ff6437de5f7dff0601379712c982485d0e11
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-20 10:39:41 -0400

    day01

---
+++ src/day01/Primitives.java
@@ -0,0 +1,29 @@
+package day01;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = (short) a; // implicit casting
+        int c = b;
+        long d = c;
+        float f = d;
+        double g =f;
+
+
+        byte x = (byte)g; // explicit casting
+        int y = (int)g;
+
+
+        char ch = 'T';
+        boolean result = true;
+        boolean result2 = false;
+
+        boolean result3 = 10 < 20;
+
+
+
+
+    }
+
+}
---
+++ src/day01/WrapperClasses.java
@@ -0,0 +1,31 @@
+package day01;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        byte b = 100;
+        int c = b;
+
+
+     // Integer x = b; // wrapper class ONLY accepts its own primitive
+
+        Integer y = c;
+     //   Double z = 10;
+
+
+        System.out.println("---------------------------------------------");
+
+        String str = "1000";
+
+        int r1 =  Integer.parseInt(str);
+        Integer r2 = Integer.valueOf(str);
+
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+
+
+
+    }
+
+}
