4461e1a8355718d4a7b66c130546a1544c940b70
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-20 14:53:21 -0400

    day01

--- src/day01/Constructors.java
+++ src/day01/Constructors.java
@@ -7,6 +7,7 @@ class Person{
     public String name;
     public char gender;
     public LocalDate dateOfBirth;
+    public static boolean isHuman = true;
 
     public Person(String name, char gender, LocalDate dateOfBirth) {
         this.name = name;
@@ -25,9 +26,24 @@ class Person{
 }
 
 
+
 public class Constructors {
 
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Ahmet", 'M', LocalDate.of(1990, 5,1));
+
+        System.out.println("person1 = " + person1);
+
+        System.out.println(person1.dateOfBirth);
+        System.out.println(Person.isHuman);
+
+
+    }
 
 
 
 }
+
+
+// instance vs static
