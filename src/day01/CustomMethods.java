f7cc43e4f6468d78eec860bf3965cbc2a0687f61
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-20 12:56:37 -0400

    day01

---
+++ src/day01/CustomMethods.java
@@ -0,0 +1,59 @@
+package day01;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomMethods<T> {  // T: Dynamic Type
+
+
+    public T max(T[] arr){
+        Arrays.sort(arr);
+        return arr[arr.length-1];
+    }
+
+    public void reverse(T[] arr){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public void reverse(List<T> list){
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+
+        CustomMethods<Integer> intArrays = new CustomMethods<>();
+
+        Integer[] arr ={1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(intArrays.max(arr) );
+
+        intArrays.reverse(arr);
+
+        CustomMethods<Double> doubleArray = new CustomMethods<>();
+        Double[] arr2 = {20.5, 20.5, 30.5};
+        doubleArray.max(arr2);
+        doubleArray.reverse(arr2);
+
+        System.out.println("-------------------------------");
+
+        String[] names = {"A", "B", "C", "D", "E"};
+
+        CustomMethods<String> stringArray = new CustomMethods<>();
+
+        stringArray.reverse(names);
+
+
+
+    }
+
+
+
+}
