7b34b596ed1b5bfc3926b0dc09da871c43f68666
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-20 11:55:35 -0400

    day01

---
+++ src/day01/Loops.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++ ){
+            System.out.println("EU5");
+        }
+
+        System.out.println("-----------------------------");
+
+        int i = 0;
+        while(i < 5){
+            System.out.println("EU5");
+            i++;
+        }
+
+
+    }
+
+}
