a14e8b9bf148d6cab3b1ec5a06c2db198535cebf
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-21 10:51:43 -0400

    Boot Camp Day02 Notes

---
+++ src/day02/inheritance_abstraction/Car.java
@@ -0,0 +1,31 @@
+package day02.inheritance_abstraction;
+
+public class Car {
+
+    public int year;
+    public String make, model, color;
+    public double price;
+
+    public Car(int year, String make, String model, String color, double price) { // initializes instances
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    public void drive(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
