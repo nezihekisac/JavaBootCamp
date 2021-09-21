ce35dc33065dc579a28e114fbe14cf93bacbeac3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-21 11:06:43 -0400

    day02

---
+++ src/day02/inheritance_abstraction/Toyota.java
@@ -0,0 +1,12 @@
+package day02.inheritance_abstraction;
+
+public class Toyota extends Car {
+
+
+    public Toyota(int year, String make, String model, String color, double price) {
+        super(year, make, model, color, price);
+    }
+
+
+
+}
