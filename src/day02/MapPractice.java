d4ee71448e0bc148cea5337bf7b25c106f1ba85a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-21 16:10:47 -0400

    day02

---
+++ src/day02/MapPractice.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class MapPractice {
+
+    public static void main(String[] args) {
+        String[] namesGroup1 = {"Oliver", "Jack", "Noah", "Breanna", "Shay","James", "Conor", "Jake", "John"};
+        Integer[] salariesGroup1 = {11000, 105000, 95000, 85000, 100000, 90000, 78000, 118000, 98000};
+
+
+
+    }
+
+}
