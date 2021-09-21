c6461a5d07e295ce1fecbe52fe9114728cdff324
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2021-09-21 11:23:51 -0400

    day02

--- src/day02/inheritance_abstraction/CarShop.java
+++ src/day02/inheritance_abstraction/CarShop.java
@@ -1,9 +1,15 @@
 package day02.inheritance_abstraction;
 
-public class CarShop {
+public class CarShop { // CarShop Has A Tesla
+
     public static void main(String[] args) {
 
-        Tesla tesla = new Tesla(2021,)
+        Tesla tesla = new Tesla(2021,"Model 3", "White", 40000 );
+
+        BMW bmw = new BMW(2020, "X6", "Red", 45000);
+
+        System.out.println(tesla);
+        System.out.println(bmw);
 
 
     }
